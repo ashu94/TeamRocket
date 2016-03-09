a18d86ef5495762af3608cb5d0e42594ca614732
ashu94 <singhjug1994@gmail.com>
2016-03-10 04:59:12 +0530

    update

--- examples/node/basic.js
+++
@@ -1,7 +0,0 @@
-var sixpack = require('../../');
-
-var session = new sixpack.Session();
-session.participate("test-exp", ["alt-one", "alt-two"], function (err, res) {
-  if (err) return console.log(err);
-  console.log(res);
-});
